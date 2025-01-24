8b79014203b2bd8b4671d5ba6495728faaa1c031
mohammadreza movahedijam <movahedijam.dev@hotmail.com>
2025-01-24 02:28:49 -0800

    Add Earth Entity

---
+++ Domain/Enum/PropertyDeedType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enum;
+
+public enum PropertyDeedType
+{
+    [Display(Name = "سند مشاع")]
+    Common,
+    [Display(Name = "سند مفروز")]
+    Mufruz,
+    [Display(Name = "سند شش دانگ")]
+    SixDang,
+    [Display(Name = "سند تک برگ")]
+    SinglePage,
+    [Display(Name = "سند وقفی")]
+    Endowment,
+    [Display(Name = "سند المثنی")]
+    Duplicate,
+    [Display(Name = "سند عرصه و اعیان")]
+    RealEstateProperty,
+    [Display(Name = "سند ورثه‌ای")]
+    Inheritance,
+    [Display(Name = "سند تفکیکی")]
+    Separation,
+    [Display(Name = "سند وکالتی")]
+    PowerAttorney,
+    [Display(Name = "سند معارض")]
+    Opposition,
+    [Display(Name = "سند بنچاق")]
+    Bench,
+    [Display(Name = "سند شورایی")]
+    Shura,
+    [Display(Name = "سند منگوله‌دار")]
+    Tambled,
+    [Display(Name = "فاقد سند")]
+    Non
+}
